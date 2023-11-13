11e85aff5e7045e8eae9ba2bbb40db580bf24f20
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2023-11-13 13:08:14 +0200

    mern project starter files

---
+++ react_apps/mern_app/client/src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="text-center mt-6 text-4xl text-blue-800">
+      Lokothwayo Dhlamini
+    </div>
+  );
+}
